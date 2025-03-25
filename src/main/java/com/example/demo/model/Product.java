b318da57a039c28c2089f90e88f572cc3acb2471
Ana Seiculescu <anaseiculescu@gmail.com>
2025-03-25 19:03:45 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="product_table")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int price;
+}
